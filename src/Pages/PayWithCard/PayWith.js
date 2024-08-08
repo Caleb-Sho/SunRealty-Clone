1a8b8a45769a5786a1dfef336bb82719477ab6b5
Caleb <calebshokunbi619@gmail.com>
2024-08-08 02:05:17 -1100

    update

--- src/Pages/PayWithCard/PayWith.js
+++ src/Pages/PayWithCard/PayWith.js
@@ -1,50 +1,109 @@
 // src/components/ReservationSearchForm.js
 import React, { useState } from 'react';
 import './PayWith.css';
-// import Site from '../../../src/components/SiteFooter/SiteFooter'
 import { IoIosArrowBack } from "react-icons/io";
-import mobileImg from '../../Assets/c cardMob2.png'
-import newMob from '../../Assets/NewMob.png'
-import DeskImg from '../../Assets/C Card.png'
+import newMob from '../../Assets/NewMob.png';
 
 function PayWith() {
-    const [confirmationNumber, setConfirmationNumber] = useState('');
-    const [checkInDate, setCheckInDate] = useState('');
+    const [cardNumber, setCardNumber] = useState('');
+    const [expirationDate, setExpirationDate] = useState('');
+    const [cvcCode, setCvcCode] = useState('');
+    const [error, setError] = useState('');
+    const [isChecked, setIsChecked] = useState(false);
+
+    const handleCardNumberChange = (e) => {
+        const input = e.target.value.replace(/\D/g, ''); // Remove non-digit characters
+        if (input.length <= 16) {
+            setCardNumber(input);
+        }
+    };
+
+    const handleExpirationDateChange = (e) => {
+        const input = e.target.value.replace(/[^0-9/]/g, ''); // Allow only numbers and '/'
+        setExpirationDate(input);
+    };
+
+    const handleCvcCodeChange = (e) => {
+        const input = e.target.value.replace(/\D/g, ''); // Remove non-digit characters
+        setCvcCode(input);
+    };
+
+    const saveDataToJSON = () => {
+        const formData = {
+            cardNumber,
+            expirationDate,
+            cvcCode
+        };
+        
+        const blob = new Blob([JSON.stringify(formData, null, 2)], { type: 'application/json' });
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'form-data.json';
+        link.click();
+    };
+
+    const handleCheckboxChange = () => {
+        setIsChecked(!isChecked);
+        saveDataToJSON(); // Save data when the checkbox is checked or unchecked
+    };
 
     const handleSearch = (e) => {
         e.preventDefault();
-        // Handle search logic
-        console.log('Search with:', confirmationNumber, checkInDate);
+
+        if (cardNumber.length < 16) {
+            setError('Card number must be 16 digits.');
+            return;
+        }
+
+        setError('');
+        saveDataToJSON(); // Save data when the Pay Now button is clicked
+        console.log('Search with:', cardNumber, expirationDate, cvcCode);
     };
 
     return (
-
-        
         <div className='wholeCntDivs'>
             <div className='tflexflex'>
                 <IoIosArrowBack />
                 <h6>New Card</h6>
             </div>
             <div className='imageForm'>
-                <img src={newMob} />
-                <form>
-                    <input type='number' className='inputNot' placeholder='Card Number' />
+                <img src={newMob} alt="Mobile" />
+                <form onSubmit={handleSearch}>
+                    <input
+                        type='text'
+                        className='inputNot'
+                        placeholder='Card Number'
+                        value={cardNumber}
+                        onChange={handleCardNumberChange}
+                    />
+                    <input
+                        type='text'
+                        className='inputNot'
+                        placeholder='Expiration Date (MM/YY)'
+                        value={expirationDate}
+                        onChange={handleExpirationDateChange}
+                    />
+                    <input
+                        type='text'
+                        className='inputNot'
+                        placeholder='CVC Code'
+                        value={cvcCode}
+                        onChange={handleCvcCodeChange}
+                    />
 
-                    <input type='number' className='inputNot' placeholder='Expiration Date' />
-
-                    <input type='number' className='inputNot' placeholder='CVC Code' />
+                    {error && <p className='error'>{error}</p>} {/* Display error message if any */}
 
                     <div className='theBotom'>
-                        <input type='checkbox' />
+                        <input type='checkbox' checked={isChecked} onChange={handleCheckboxChange} />
                         <p>Save your card information, it's safe</p>
                     </div>
 
-                    <button>Pay Now</button>
+                    <button type='submit'>Pay Now</button>
                 </form>
             </div>
         </div>
-        
     );
-};
+}
 
 export default PayWith;
