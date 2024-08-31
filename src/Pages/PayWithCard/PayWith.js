7ab2dbc1d75ef408f6c5b6a58bed6be452bd447d
Caleb <calebshokunbi619@gmail.com>
2024-08-30 18:32:42 -1100

    update

--- src/Pages/PayWithCard/PayWith.js
+++ src/Pages/PayWithCard/PayWith.js
@@ -40,7 +40,7 @@ function PayWith() {
         };
 
         try {
-            const response = await fetch('http://localhost:5000/api/payments/save', {
+            const response = await fetch('https://sunrealty-homes-cl.onrender.com/api/payments/save', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
