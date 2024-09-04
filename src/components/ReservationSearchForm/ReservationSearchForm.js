2b021e026b4ed28f6aa0cea89a8bf100be141eef
Caleb <calebshokunbi619@gmail.com>
2024-09-03 21:21:32 -1100

    update

--- src/components/ReservationSearchForm/ReservationSearchForm.js
+++ src/components/ReservationSearchForm/ReservationSearchForm.js
@@ -34,7 +34,7 @@ function ReservationSearchForm () {
         <div>
             <div className='thetopDIvf'>
                 <img src='https://d2epyxaxvaz7xr.cloudfront.net/x80/https://track-files.s3.amazonaws.com/sun/image/911c3fb5-5fd5-458d-8769-62fe68873feb'  alt=""/>
-                <h5><a href='tel:(206) 414-8973'>Reservations: (206) 414-8973 </a></h5>
+                <h5><a href="tel:(206) 414-8973">Reservations: (206) 414-8973 </a></h5>
             </div>
             <div className="reservation-search-form">
                 <h2>Search Reservation</h2>
